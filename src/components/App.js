ea7415cfdffecb6adedae18b98d4c21b3f8b22ab
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-11-25 12:16:29 +0000

    Pulling from multiple context

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContexts";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -19,9 +20,11 @@ class App extends React.Component {
         />
         <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
         {/* here value is dynamic */}
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
         {/* default value of english is overriden to dutch */}
         <LanguageContext.Provider value="dutch">
           <UserCreate />
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,27 @@
 import React from "react";
 import LanguageContexts from "../contexts/LanguageContexts";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   renderSubmit = value => {
     return value === "english" ? "Submit" : "Voorlaggen";
   };
 
-  render() {
+  renderColor = color => {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContexts.Consumer>
           {value => this.renderSubmit(value)}
         </LanguageContexts.Consumer>
       </button>
+    );
+  };
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderColor(color)}
+      </ColorContext.Consumer>
     );
   }
 }
