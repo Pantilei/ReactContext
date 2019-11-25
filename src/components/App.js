6aee48a5da33cc31c2bf137dc6dffcda0ee25f5c
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-11-25 11:51:56 +0000

    LanguageContext.Provider added

--- src/components/App.js
+++ src/components/App.js
@@ -18,9 +18,16 @@ class App extends React.Component {
           onClick={() => this.onLanguageChange("english")}
         />
         <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
+        {/* here value is dynamic */}
         <LanguageContext.Provider value={this.state.language}>
           <UserCreate />
         </LanguageContext.Provider>
+        {/* default value of english is overriden to dutch */}
+        <LanguageContext.Provider value="dutch">
+          <UserCreate />
+        </LanguageContext.Provider>
+        {/* default values of context will be used */}
+        <UserCreate />
       </div>
     );
   }
