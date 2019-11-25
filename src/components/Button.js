58d82b95ec0cd57067a50792a9b81f1ddecf5b21
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-11-25 12:01:20 +0000

    Getting information with LanguageContext.Consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,18 @@ import React from "react";
 import LanguageContexts from "../contexts/LanguageContexts";
 
 class Button extends React.Component {
-  static contextType = LanguageContexts;
+  renderSubmit = value => {
+    return value === "english" ? "Submit" : "Voorlaggen";
+  };
+
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorlagen";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContexts.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContexts.Consumer>
+      </button>
+    );
   }
 }
 
